92f7bf2b930b5e68690e94837332fca4b2d263ce
avner <avner@avner-HP-Pavilion-dm4-Notebook-PC>
2018-08-01 00:05:45 +0300

    bh

--- qtree_project/qtree.f95
+++ qtree_project/qtree.f95
@@ -1,4 +1,7 @@
+
 module qtree
+implicit none
+
 
 
 
@@ -14,8 +17,26 @@ module RK4
 
 end module RK4
 
+module galaxy
+implicit none
+contains
+    subroutine initialize(v)
+    real(8), intent(IN) :: v
+    real(8), parameter ::  parsec = 3.085677581d16, R = parsec*50d3, m_sun = 2d30, M = m_sun*1d11, n=2000
+    integer :: i
+    real(8) :: grid_distance, x_grid(n), y_grid(n), z_grid(n)
+    grid_distance = R/n
+    x_grid = (/ (i,i=0,n)*grid_distance /)
+    y_grid = (/ (i,i=0,n)*grid_distance /)
+    z_grid = (/ (i,i=0,n)*grid_distance /)
 
-program qtree
+    end subroutine intialize
+
+
+
+end module galaxy
+
+program gravitational_dynamics
     implicit none
     print *, 'hello World '
-end program qtree
+end program gravitational_dynamics
