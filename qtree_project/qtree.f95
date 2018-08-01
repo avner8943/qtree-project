8a14f779657648b12c455a3539a25a6697c0a364
avner <avner8943@gmail.com>
2018-08-01 12:31:42 +0300

    fvb

--- qtree_project/qtree.f95
+++ qtree_project/qtree.f95
@@ -1,7 +1,7 @@
 
 module qtree
-implicit none
-s
+    implicit none
+
 
 
 
@@ -18,17 +18,33 @@ module RK4
 end module RK4
 
 module galaxy
-implicit none
+    implicit none
 contains
     subroutine initialize(v)
-    real(8), intent(IN) :: v
-    real(8), parameter ::  parsec = 3.085677581d16, R = parsec*50d3, m_sun = 2d30, M = m_sun*1d11, n=2000
-    integer :: i
-    real(8) :: grid_distance, x_grid(n), y_grid(n), z_grid(n)
-    grid_distance = R/n
-    x_grid = (/ (i,i=0,n)*grid_distance /)
-    y_grid = (/ (i,i=0,n)*grid_distance /)
-    z_grid = (/ (i,i=0,n)*grid_distance /)
+        real(8), intent(IN) :: v
+        real(8), parameter ::  parsec = 3.085677581d16, R = parsec*50d3, m_sun = 2d30, M = m_sun*1d11, n=2d3, n2
+        real(8), allocatable :: pos(:,3), velocity(:,3)
+        integer :: i, j, k, l
+        real(8) :: grid_distance, x_grid(n), y_grid(n), z_grid(n)
+        grid_distance = R/n
+        x_grid = (/ (i,i=0,n)*grid_distance-1d3 /)
+        y_grid = (/ (i,i=0,n)*grid_distance-1d3 /)
+        z_grid = (/ (i,i=0,n)*grid_distance-1d3 /)
+
+        l=1
+        do i=1,n
+            do k=1,n
+                do j=1,n
+                    if (x_grid(i)**2+y_grid(k)**2+z_grid(j)**2<R**2) then
+                        pos(l,:) = (x_grid(i), y_grid(k), z_grid(j))
+                        l = l+1
+                    end if
+                end do
+            end do
+        end do
+
+       n2 = floor(l/2.0_8)
+
 
     end subroutine intialize
 
