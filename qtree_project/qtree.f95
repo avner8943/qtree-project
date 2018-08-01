9a7d5a732d8b35fd9c07daf4c47956f0895a9adf
avner <avner8943@gmail.com>
2018-08-01 14:30:19 +0300

    rtsh

--- qtree_project/qtree.f95
+++ qtree_project/qtree.f95
@@ -20,39 +20,50 @@ end module RK4
 module galaxy
     implicit none
 contains
-    subroutine initialize(v)
+    subroutine initialize(v,grid,pos,m, velocity)
         real(8), intent(IN) :: v
-        real(8), parameter ::  parsec = 3.085677581d16, R = parsec*50d3, m_sun = 2d30, M_tot = m_sun*1d11, n=2d3,  Pi = 3.1415927
-        real(8), allocatable :: pos(:,3), velocity(:,3), temp(:,3)
+        integer, parameter :: n=2000
+        real(8), parameter ::  parsec = 3.085677581d16, R = parsec*50d3, m_sun = 2d30, M_tot = m_sun*1d11,  Pi = 3.1415927
+        real(8), allocatable, intent(OUT) :: pos(:,:), velocity(:,:)
+        real(8), intent(OUT) :: m, grid(n,3)
+        real(8), allocatable :: temp(:,:)
         integer :: i, j, k, l, n2
-        real(8) :: grid_distance, x_grid(n), y_grid(n), z_grid(n), sigma
-        grid_distance = R/n
-        x_grid = (/ (i,i=0,n)*grid_distance-1d3 /)
-        y_grid = (/ (i,i=0,n)*grid_distance-1d3 /)
-        z_grid = (/ (i,i=0,n)*grid_distance-1d3 /)
 
+        real(8) :: grid_distance, sigma,x1,x2,y1,y2,y3
+
+        grid_distance = R/n
+        grid(:,1) = (/ (i,i=1,n) /)*grid_distance-1d3
+        grid(:,2) = (/ (i,i=1,n) /)*grid_distance-1d3
+        grid(:,3) = (/ (i,i=1,n) /)*grid_distance-1d3
+
+        allocate(pos(int(1d4),3))
+        allocate(temp(int(1d4),3))
         l=1
         do i=1,n
             do k=1,n
                 do j=1,n
-                    if (x_grid(i)**2+y_grid(k)**2+z_grid(j)**2<R**2) then
-                        pos(l,:) = (x_grid(i), y_grid(k), z_grid(j))
+                    if (grid(i,1)**2+grid(k,2)**2+grid(j,3)**2<R**2) then
+                        pos(l,:) = (/ grid(i,1), grid(k,2), grid(j,3) /)
                         l = l+1
                     end if
                 end do
             end do
         end do
 
-        if (mod(f,2)==0) then
+        if (mod(l,2)==0) then
             call move_alloc(pos,temp)
             allocate (pos(l-2,3))
             pos(:,:) = temp(1:l-2,:)
             n2 = l-2
         else
+            call move_alloc(pos,temp)
+            allocate (pos(l-1,3))
+            pos(:,:) = temp(1:l-1,:)
             n2 = l-1
         end if
 
-       sigma = v/sqrt(3)
+       allocate(velocity(n2,3))
+       sigma = v/sqrt(3._8)
        do i=1,n2
        call random_number(x1)
        call random_number(x2)
@@ -67,13 +78,26 @@ contains
 
        m = M_tot/n2
 
-    end subroutine intialize
+    end subroutine initialize
 
 
 
 end module galaxy
 
 program gravitational_dynamics
-    implicit none
-    print *, 'hello World '
+    use galaxy
+    real(8) :: v=85d3, m
+    real(8), allocatable :: grid(:,:), pos(:,:), velocity(:,:)
+    integer :: i,n
+
+    call initialize(v,grid,pos,m, velocity)
+!    n =size(velocity,1)
+!    print *, n
+!    open(unit=1 , file = 'test.csv')
+!    do i=1,n
+!    write (1, '(3es12.4)')velocity(i,1),velocity(i,2),velocity(i,3)
+!    end do
+!    close (unit=1)
+
+
 end program gravitational_dynamics
